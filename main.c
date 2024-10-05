ab263d4ed4bf25a7c17d274e0adf0ecb891d9770
Erick B. <erick.cr12@outlook.com>
2024-10-04 21:40:31 -0600

    Se realiza json y su parseo. Estructura UserData contiene los datos (Singleton)

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include "tests/ship_test.h"
 #include "tests/thread_test.h"
 #include "tests/mutex_test.h"
+#include "tests/config_test.h"
 
 
 int main() {
     //Si quieren correr pruebas en el main, crear una función aparte en un archivo aparte en tests/ y llamarlas aquí
     //shipTest();
     //hiloTest();
-    mutexTest();
+    //mutexTest();
+    configTest();
 
     return 0;
 }
